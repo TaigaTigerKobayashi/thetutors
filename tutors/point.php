1ad169ef446b3e60788e53540c005398d6ecb7c8
TaigaTigerKobayashi <62683204+TaigaTigerKobayashi@users.noreply.github.com>
2020-06-20 23:52:02 +0900

    06202300 taiga updated

--- tutors/point.php
+++ tutors/point.php
@@ -1,12 +1,53 @@
+<?php
+session_start();
+include("funcs.php");
+$pdo = db_conn();
+
+$sql = "SELECT TUTOR,COUNT(TUTOR) AS COUNT FROM CALENDAR_TABLE GROUP BY TUTOR";
+$stmt = $pdo->prepare($sql);
+$status = $stmt->execute();
+
+//３．データ表示
+$view = "";
+if ($status == false) {
+    sql_error($stmt);
+} else {
+    //Selectデータの数だけ自動でループしてくれる
+    //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $view .= '<P>';
+        $view .= $result["TUTOR"] . ":" . $result["COUNT"];;
+        $view .= '　';
+        $view .= '</p>';
+    }
+
+}
+?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>ポイント表示</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
 </head>
-<body>
-<a href="logout.php">ログアウト</a>
-  <h1>ポイント確認画面です</h1>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<div>
+    <h1> POINT</h1>
+    <div class="container jumbotron"><?php echo $view; ?></div>
+</div>
+<!-- Main[End] -->
+
 </body>
 </html>
